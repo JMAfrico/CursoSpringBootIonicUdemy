2082b225ad05f638854980d35a3738c995ae24ca
JMAfrico <joao_marcossilva@hotmail.com>
2021-08-27 12:47:18 -0300

    Listando Categorias com Paginacao

--- src/main/java/com/jmarcostech/cursomc/CursomcApplication.java
+++ src/main/java/com/jmarcostech/cursomc/CursomcApplication.java
@@ -64,6 +64,14 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Cama mesa e banho");
+		Categoria cat4 = new Categoria(null,"Eletromesticos");
+		Categoria cat5 = new Categoria(null,"Papelaria");
+		Categoria cat6 = new Categoria(null,"Eletroeletronicos");
+		Categoria cat7 = new Categoria(null,"Vestuario");
+		Categoria cat8 = new Categoria(null,"Midias digitais");
+		Categoria cat9 = new Categoria(null,"Periféricos");
+		Categoria cat10 = new Categoria(null,"Outros");
 		
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
@@ -76,7 +84,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		catrepo.saveAll(Arrays.asList(cat1,cat2));
+		catrepo.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10));
 		prodrepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 		//CIDADE COM ESTADOS//
