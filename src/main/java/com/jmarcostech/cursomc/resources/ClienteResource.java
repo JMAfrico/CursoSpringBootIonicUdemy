88426ec201ba86a455a9685be5b3e4d4feeb9d6b
JMAfrico <joao_marcossilva@hotmail.com>
2021-08-29 16:57:16 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/jmarcostech/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jmarcostech/cursomc/resources/ClienteResource.java
@@ -1,14 +1,22 @@
 package com.jmarcostech.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jmarcostech.cursomc.domain.Cliente;
+import com.jmarcostech.cursomc.dto.ClienteDTO;
 import com.jmarcostech.cursomc.services.ClienteService;
 
 @RestController
@@ -25,4 +33,52 @@ public class ClienteResource {
 		
 	}
 	
+	//REQUEST QUE USA O SERVICE PARA ALTERAR CATEGORIA POR ID
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+		Cliente obj = clienteservice.fromDTO(objDTO);
+		obj.setId(id);
+		obj = clienteservice.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//REQUEST QUE USA O SERVICE PARA DELETAR CATEGORIA POR ID
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteservice.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//REQUEST QUE USA O SERVICE PARA LISTAR TODAS AS CATEGORIAS
+	/*
+	 * @RequestMapping(method = RequestMethod.GET) public
+	 * ResponseEntity<List<Cliente>> findAll() { List<Cliente> list =
+	 * clienteservice.findAll(); return ResponseEntity.ok().body(list);
+	 * 
+	 * }
+	 */
+	
+	//REQUEST QUE USA O SERVICE PARA LISTAR TODAS AS CATEGORIAS, SEM MOSTRAR OS DEPENDENTES
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = clienteservice.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj))
+				.collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
+	
+	//REQUEST QUE USA O SERVICE PARA LISTAR TODAS AS CATEGORIAS, SEM MOSTRAR OS DEPENDENTES, POR PAGINAS
+	@RequestMapping(value = "/page",method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Cliente> list = clienteservice.findPage(page,linesPerPage,orderBy,direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
 }
--- src/main/java/com/jmarcostech/cursomc/services/ClienteService.java
+++ src/main/java/com/jmarcostech/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.jmarcostech.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jmarcostech.cursomc.domain.Cliente;
+import com.jmarcostech.cursomc.dto.ClienteDTO;
 import com.jmarcostech.cursomc.repositories.ClienteRepository;
+import com.jmarcostech.cursomc.services.exceptions.DataIntegrityException;
 import com.jmarcostech.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +28,45 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Cliente: " + Cliente.class.getName()));
 	
 	}
+	
+	//SERVICE PARA ALTERAR CATEGORIA SE ELA JÁ EXISTIR
+		public Cliente update(Cliente obj) {
+			Cliente newobj = find(obj.getId());
+			updateData(newobj,obj);
+			return clienterepository.save(newobj);
+		}
+		
+		//SERVICE PARA DELETAR CATEGORIA SE ELA JÁ EXISTIR
+		public void delete(Integer id) {
+			find(id);
+			try {
+			clienterepository.deleteById(id);
+			}
+			catch (DataIntegrityViolationException e) {
+				throw new DataIntegrityException("Não é possível deletar cliente que possui compras efetuadas");
+			}
+		}
+		
+		//SERVICE PARA LISTAR TODAS AS CATEGORIAS
+		public List<Cliente> findAll() {
+			return clienterepository.findAll();
+		}
+		
+		//SERVICE PARA PAGINAR AS CATEGORIAS
+		public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
+			return clienterepository.findAll(pageRequest);
+		}
+		
+		//SERVICE QUE PASSA OS PARÂMETROS DA CATEGORIA, PARA CATEGORIADTO, PARA PODER SER VALIDADO PELO REQUEST
+		public Cliente fromDTO(ClienteDTO objDTO) {
+			//return new Cliente(objDTO.getId(),objDTO.getNome());
+			return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+		}
+		
+		private void updateData(Cliente newObj, Cliente obj) {
+			newObj.setNome(obj.getNome());
+			newObj.setEmail(obj.getEmail());
+			
+		}
 }
